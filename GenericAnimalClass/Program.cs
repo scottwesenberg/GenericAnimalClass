6b812171dba39b3d4e6a12abb5e1e8a8bb162823
wesen <wesen@GAMEPC-SCOOT>
2023-03-06 14:57:20 -0500

    Add project files.

---
+++ GenericAnimalClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GenericAnimalClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create generic animal object
+            Animal<string> myAnimal = new Animal<string>("Blue Penguin");
+
+            // Add animal information
+            string animalHabitat = "New Zealand";
+            bool endangered = false;
+            bool extinct = false;
+            int averageLifespan = 6;
+
+            // Print out animal information
+            Console.WriteLine($"Animal name: {myAnimal.getAnimal()}");
+            Console.WriteLine($"Habitat: {animalHabitat}");
+            Console.WriteLine($"Endangered: {endangered}");
+            Console.WriteLine($"Extinct: {extinct}");
+            Console.WriteLine($"Average lifespan: {averageLifespan}");
+        }
+    }
+    // Create generic class 
+    public class Animal<T>
+    {
+        private T data;
+        //Set animalName to data
+        public Animal(T animalName)
+        {
+            data = animalName;
+        }
+        public T getAnimal()
+        {
+            return data;
+        }
+    }
+}
